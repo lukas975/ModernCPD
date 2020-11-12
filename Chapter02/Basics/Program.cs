e66188b8270d47c6ce47361c4cbecaaa30dafd20
Luka <sabo.luka@gmail.com>
2020-11-12 14:47:16 +0100

    chapter 2

---
+++ Chapter02/Basics/Program.cs
@@ -0,0 +1,55 @@
+﻿using System; 
+using System.Linq;
+using System.Reflection;
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            // declare some unused variables using types
+            // in additional assemblies
+            System.Data.DataSet ds;
+            System.Net.Http.HttpClient client;
+
+            // loop through the assemblies that this app references
+            foreach (var r in Assembly.GetEntryAssembly().GetReferencedAssemblies())
+            {
+                // load the assembly so we can read its details
+                var a = Assembly.Load(new AssemblyName(r.FullName));
+
+                // declare a variable to count the number of methods
+                int methodCount = 0;
+                
+                // loop through all the types in the assembly
+                foreach (var t in a.DefinedTypes)
+                {
+                    // add up the counts of methods
+                    methodCount += t.GetMethods().Count();
+                }
+
+                // output the count of types and their methods
+                Console.WriteLine("{0:N0} types with {1:N0} methods in {2} assembly.", arg0: a.DefinedTypes.Count(), arg1: methodCount, arg2: r.Name);
+            }
+
+            // let the heightInMetres variable become equal to the value 1.88
+            double heightInMetres = 1.88;
+            Console.WriteLine($"The variable {nameof(heightInMetres)} has the value {heightInMetres}.");
+
+            char letter = 'A'; // assigning literal characters
+            char digit = '1';
+            char symbol = '$';
+            //char userChoice = GetKeystroke(); // assigning from a function
+
+            string firstName = "Bob"; // assigning literal strings
+            string lastName = "Smith";
+            string phoneNumber = "(215) 555-4256";
+            // assigning a string returned from a function call
+            //string address = GetAddressFromDatabase(id: 563);
+
+            string fullNameWithTabSeparator = "Bob\tSmith";
+            string filePath = @"C:\televisions\sony\bravia.txt";
+        }   
+    }
+}
---
+++ Chapter02/NullHandling/Program.cs
@@ -0,0 +1,49 @@
+﻿#nullable enable
+using System;
+
+namespace NullHandling
+{
+    class Address
+    {
+        public string? Building;
+        public string Street = string.Empty;
+        public string City = string.Empty;
+        public string Region = string.Empty;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int thisCannotBeNull = 4;
+            //thisCannotBeNull = null; // compile error!
+            int? thisCouldBeNull = null;
+            Console.WriteLine(thisCouldBeNull);
+            Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+            thisCouldBeNull = 7;
+            Console.WriteLine(thisCouldBeNull);
+            Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+            var address = new Address();
+            address.Building = null;
+            address.Street = null;
+            address.City = "London";
+            address.Region = null;
+
+            if (thisCouldBeNull != null)
+            {
+                // access a member of thisCouldBeNull
+                int length = thisCouldBeNull.Length; // could throw exception ...
+            }
+
+            string authorName = null;
+            // the following throws a NullReferenceException
+            int x = authorName.Length;
+            // instead of throwing an exception, null is assigned to y
+            int? y = authorName?.Length;
+
+            // result will be 3 if authorName?.Length is null
+            var result = authorName?.Length ?? 3;
+            Console.WriteLine(result);
+        }
+    }
+}
